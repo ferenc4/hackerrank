c3548202579d53546e7e9650d03516b18aa2f886
Ferenc <ferenc4.fazekas@gmail.com>
2016-06-30 15:31:50 +1000

    BigInteger solution

--- src/main/java/Mathematics/Algebra/stepping_stones_game/Solution.java
+++ src/main/java/Mathematics/Algebra/stepping_stones_game/Solution.java
@@ -1,13 +1,28 @@
-
 package Mathematics.Algebra.stepping_stones_game;
 
 /**
  * Created by Ferenc on 6/30/2016.
  */
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Solution {
+
+    public static BigInteger sqrt(BigInteger x) {
+        BigInteger div = BigInteger.ZERO.setBit(x.bitLength() / 2);
+        BigInteger div2 = div;
+
+        // Loop until we hit the same value twice in a row,
+        // or keep alternating.
+        while(true) {
+            BigInteger y = div.add(x.divide(div)).shiftRight(1);
+            if (y.equals(div) || y.equals(div2))
+                return y;
+            div2 = div;
+            div = y;
+        }
+    }
 
     public static void main(String[] args) {
         int games = 0;
@@ -15,14 +30,17 @@ public class Solution {
         games = sc.nextInt();
         for (int i = 0; i < games; i++) {
             if (sc.hasNextInt()) {
-                long currentBlock = 0;
-                long goalBlock = sc.nextInt();
-                long stepSize = 1;
-                for (; currentBlock < goalBlock; stepSize++) {
-                    currentBlock += stepSize;
-                }
-                if (currentBlock == goalBlock) {
-                    System.out.println("Go On Bob " + (stepSize - 1));
+                BigInteger goalBlock = sc.nextBigInteger();
+                //m = (sqrt(8n+1) - 1) / 2
+                BigInteger calcPart = goalBlock
+                        .multiply(BigInteger.valueOf(8))
+                        .add(BigInteger.ONE);
+                BigInteger steps = sqrt(calcPart);
+                if (steps.multiply(steps).equals(calcPart)) {
+                    steps = steps
+                            .subtract(BigInteger.ONE)
+                            .divide(BigInteger.valueOf(2));
+                    System.out.println("Go On Bob "+steps.toString());
                 } else {
                     System.out.println("Better Luck Next Time");
                 }
