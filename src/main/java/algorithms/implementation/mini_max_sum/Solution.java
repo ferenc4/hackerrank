6928dd0cbf9764a8cdb3bdbb95e26242b307ed1c
Ferenc <ferenc4.fazekas@gmail.com>
2017-02-18 17:42:12 +1100

    added a new question

---
+++ src/main/java/algorithms/implementation/mini_max_sum/Solution.java
@@ -0,0 +1,22 @@
+package algorithms.implementation.mini_max_sum;
+
+import java.util.Scanner;
+
+/**
+ * Created by Ferenc on 6/30/2016.
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int tests = sc.nextInt();
+            for (int i = 0; i < tests; i++) {
+                run(sc);
+            }
+        }
+    }
+
+    public static void run(Scanner sc) {
+    }
+}
