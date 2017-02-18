587e28905bc43b1910f0a8b51ac41ea6fa0604e0
Ferenc <ferenc4.fazekas@gmail.com>
2017-02-18 18:52:20 +1100

    finished challenge

--- src/main/java/algorithms/implementation/mini_max_sum/Solution.java
+++ src/main/java/algorithms/implementation/mini_max_sum/Solution.java
@@ -1,5 +1,6 @@
 package algorithms.implementation.mini_max_sum;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
@@ -9,14 +10,21 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        if (sc.hasNextInt()) {
-            int tests = sc.nextInt();
-            for (int i = 0; i < tests; i++) {
-                run(sc);
+        BigInteger sum = BigInteger.ZERO;
+        BigInteger smallest = null;
+        BigInteger greatest = null;
+        if (sc.hasNextBigInteger()) {
+            BigInteger currentNumber = sc.nextBigInteger();
+            smallest = currentNumber;
+            greatest = currentNumber;
+            sum = sum.add(currentNumber);
+            while (sc.hasNextBigInteger()) {
+                currentNumber = sc.nextBigInteger();
+                smallest = currentNumber.min(smallest);
+                greatest = currentNumber.max(greatest);
+                sum = sum.add(currentNumber);
             }
         }
-    }
-
-    public static void run(Scanner sc) {
+        System.out.println(sum.subtract(greatest) + " " + sum.subtract(smallest));
     }
 }
---
+++ src/test/java/algorithms/implementation/mini_max_sum/SolutionTest.java
@@ -0,0 +1,14 @@
+package algorithms.implementation.mini_max_sum;
+
+import org.junit.Test;
+import testutils.Question;
+
+/**
+ * Created by Ferenc on 6/30/2016.
+ */
+public class SolutionTest {
+    @Test
+    public void runSolution() throws NoSuchMethodException {
+        new Question(Solution.class).testAnswer();
+    }
+}
