37d79b8b3d903dc64bb29ae32d40edbffbc3b36f
Ferenc <ferenc4.fazekas@gmail.com>
2017-02-22 20:12:51 +1100

    added new template solution

---
+++ src/main/java/TemplateSolution2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Created by Ferenc on 2/22/2016.
+ */
+public class TemplateSolution2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final Integer count = sc.nextInt();
+        for (int i = 0; i < count; i++) {
+            sc.nextInt();
+        }
+    }
+}
